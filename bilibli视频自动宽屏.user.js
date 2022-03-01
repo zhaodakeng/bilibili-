8719c3299e1af75e879f392cca9843fd65d22dbf
jf <a961011576@qq.com>
2022-03-01 14:58:21 +0800

    删除了宽屏后自动定位的代码。

--- bilibli视频自动宽屏.user.js
+++ bilibli视频自动宽屏.user.js
@@ -1,61 +1,44 @@
 // ==UserScript==
 // @name         bilibili视频自动宽屏
 // @namespace    http://zjfhome.320.io
-// @version      0.3
+// @version      1.2
 // @description  bilibili视频自动宽屏显示并且自动播放
-// @author       zjf-a961011576@qq.com
-// @match        *://*.bilibili.com/*
+// @author       Email：a961011576@qq.com
+// @match        *://*.bilibili.com/video/*
 // @grant        none
 // @run-at document-idle
+// @license MIT
 // ==/UserScript==
-
-
 (function () {
     'use strict';
-    let videodom = undefined
-    /*定时器循环ms*/
-    let time = 10
-    /*播放视频*/
-    let videoPlay=()=> {
-        let interval = 0
-        let playDom = undefined
-        /*尝试播放视频*/
-        let play = () => {
+    let observer = new MutationObserver(()=>{
+        let danmakuBox =document.querySelector(".danmaku-box");
+        let mt = window.getComputedStyle(danmakuBox,undefined).getPropertyValue("margin-top")
+        // console.log('监听到了变化')
+        if(mt.includes('0')){
+            init()
+        }
+    });
+    let totalNums = document.querySelector(".tit");
+    observer.observe(totalNums, {
+        'childList': true,
+        'subtree': true
+    })
+    let init=()=>{
+        /*定时器循环ms*/
+        let time = 50
+        let interval = setInterval(()=>{
+            console.log(interval)
             //宽屏DOM
-            playDom = videodom.onloadstart = document.getElementsByClassName('bilibili-player-iconfont bilibili-player-iconfont-widescreen-off player-tooltips-trigger')[0]
+            let playDom = document.querySelector('.bilibili-player-iconfont.bilibili-player-iconfont-widescreen-off.player-tooltips-trigger') //宽屏dom
+            //playDom = videodom.onloadstart = document.querySelector('.bilibili-player-iconfont.bilibili-player-iconfont-web-fullscreen-off.player-tooltips-trigger')//网页全屏dom
+            /*尝试宽屏*/
             if (playDom) {
-                //结束定时
-                if (interval)
-                    clearInterval(interval)
+                window.clearInterval(interval)
                 playDom.click()
-                videodom.play()
-                try {
-                    document.querySelector("#playerWrap").scrollIntoView({behavior: "smooth"})
-                }catch (e) {
-                }
+                document.querySelector(".danmaku-wrap").style.display="none"
             }
-        }
-        play()
-        //如果第一次尝试执行后未成功时开启定时
-        if(!playDom){
-            interval = setInterval(() => {
-                play()
-            }, time)
-        }
+        },time)
+    }
 
-    }
-    /*获取videoDom*/
-    let getVideoDom = () => {
-        videodom = document.querySelector('video')
-        if (videodom) {
-            if(videoDomInterval) //当有定时器时调用
-                clearInterval(videoDomInterval)
-            videoPlay()
-        }
-    }
-    let videoDomInterval = 0
-    getVideoDom()
-    if(!videodom)//初始化未成功时开启定时器
-        videoDomInterval = setInterval(getVideoDom, time)
 })();
-
