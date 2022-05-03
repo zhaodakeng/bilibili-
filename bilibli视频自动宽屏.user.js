3d2e15c383d38391a4d57a822562891509a9e945
zhaodakeng <42629715+zhaodakeng@users.noreply.github.com>
2022-05-03 14:13:59 +0800

    Add files via upload
    修复番剧无法自动宽屏的问题

--- bilibli视频自动宽屏.user.js
+++ bilibli视频自动宽屏.user.js
@@ -1,44 +1,56 @@
 // ==UserScript==
 // @name         bilibili视频自动宽屏
 // @namespace    http://zjfhome.320.io
-// @version      1.2
+// @version      1.3
 // @description  bilibili视频自动宽屏显示并且自动播放
 // @author       Email：a961011576@qq.com
 // @match        *://*.bilibili.com/video/*
+// @match        *://*.bilibili.com/watchlater/*
+// @match        *://*.bilibili.com/bangumi/play/*
+// @match        *://*.bilibili.com/medialist/play/*
 // @grant        none
 // @run-at document-idle
 // @license MIT
 // ==/UserScript==
 (function () {
     'use strict';
-    let observer = new MutationObserver(()=>{
-        let danmakuBox =document.querySelector(".danmaku-box");
-        let mt = window.getComputedStyle(danmakuBox,undefined).getPropertyValue("margin-top")
+    let observer = new MutationObserver(() => {
         // console.log('监听到了变化')
-        if(mt.includes('0')){
-            init()
-        }
+        init()
+
     });
-    let totalNums = document.querySelector(".tit");
-    observer.observe(totalNums, {
+    let node = undefined
+    if (window.location.href.includes('bilibili.com/bangumi/play'))
+        node = document.querySelector(".media-title")
+    else
+        node = document.querySelector(".tit")
+
+    observer.observe(node, {
         'childList': true,
         'subtree': true
     })
-    let init=()=>{
+    let init = () => {
         /*定时器循环ms*/
-        let time = 50
-        let interval = setInterval(()=>{
-            console.log(interval)
+        const time = 50
+        const href = window.location.href
+        let interval = setInterval(() => {
+            // console.log(interval)
             //宽屏DOM
-            let playDom = document.querySelector('.bilibili-player-iconfont.bilibili-player-iconfont-widescreen-off.player-tooltips-trigger') //宽屏dom
+            let playDom = undefined
+
+            if (href.includes('bilibili.com/bangumi/play'))
+                playDom = document.querySelector('.squirtle-video-widescreen')
+            else
+                playDom = document.querySelector('.bilibili-player-iconfont.bilibili-player-iconfont-widescreen-off.player-tooltips-trigger') //宽屏dom
             //playDom = videodom.onloadstart = document.querySelector('.bilibili-player-iconfont.bilibili-player-iconfont-web-fullscreen-off.player-tooltips-trigger')//网页全屏dom
             /*尝试宽屏*/
             if (playDom) {
                 window.clearInterval(interval)
-                playDom.click()
-                document.querySelector(".danmaku-wrap").style.display="none"
+                if(!document.querySelector('.squirtle-video-widescreen.squirtle-video-item.active'))
+                    playDom.click()
+                document.querySelector(".danmaku-wrap").style.display = "none"
             }
-        },time)
+        }, time)
     }
 
 })();
